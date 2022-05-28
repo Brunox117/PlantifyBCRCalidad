8b24b3a0efa83b13d05f88c6b4e5d168851482e9
Bruno <omarjm0910@gmail.com>
2022-05-28 13:12:40 -0500

    creamos el proyecto de calidad

---
+++ app/src/main/java/mx/itesm/bcr/plantifybcr/Usuario.kt
@@ -0,0 +1,9 @@
+package mx.itesm.bcr.plantifybcr
+
+data class Usuario(
+    var tokken: String="",
+    var nombre: String="",
+    var correo: String="",
+    var tipoUsuario: String=""
+)
+//Creamos el tipo usuario por si se usa en un futuro
